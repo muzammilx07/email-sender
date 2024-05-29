80bcdaa79041f5f6269b31dcfaea2c96871376d7
Mohammed Muzammil Shareef <mohdmuzzammil633@gmail.com>
2024-05-30 00:20:50 +0530

    Update mailConfig.js

--- backend/config/mailConfig.js
+++ backend/config/mailConfig.js
@@ -5,8 +5,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'process.env.SMTP_EMAIL',
-        pass: 'process.env.SMTP_PASSWORD'
+        user: process.env.SMTP_EMAIL,
+        pass: process.env.SMTP_PASSWORD
     }
 });
 
